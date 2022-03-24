6a2871696436e457de76a1cbaeb982d9c443cab4
anthonym995 <manthonysuresh@gmail.com>
2022-03-24 09:02:40 +0530

    updated api

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -12,28 +12,28 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   postEmployee(data:any){
-   return this.http.post<any>('https://demojsonserver.herokuapp.com/posts', data).pipe(map((res:any) => {
+   return this.http.post<any>('https://demojsserver.herokuapp.com/posts', data).pipe(map((res:any) => {
       console.log('Res',res);
       return res;
     }))
   }
 
   getEmployee(){
-   return this.http.get<any>('https://demojsonserver.herokuapp.com/posts').pipe(map((res:any) => {
+   return this.http.get<any>('https://demojsserver.herokuapp.com/posts').pipe(map((res:any) => {
     console.log('Res',res);
       return res;
     }))
   }
 
   updateEmployee(data:any, id: number){
-   return this.http.put<any>('https://demojsonserver.herokuapp.com/posts/'+id, data).pipe(map((res:any) => {
+   return this.http.put<any>('https://demojsserver.herokuapp.com/posts/'+id, data).pipe(map((res:any) => {
     console.log('Res',res);
       return res;
     }))
   }
 
   deleteEmployee(id: number){
-  return this.http.delete<any>('https://demojsonserver.herokuapp.com/posts/'+id).pipe(map((res:any) => {
+  return this.http.delete<any>('https://demojsserver.herokuapp.com/posts/'+id).pipe(map((res:any) => {
     console.log('Res',res);
       return res;
     }))
